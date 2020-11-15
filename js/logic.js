63fd2fc99be195fc4985e67c959d4669fac8ed1b
Troy Dorman <troydorman@gmail.com>
2020-11-14 21:02:46 -0600

    got fixed navbar and basic sections set up

---
+++ js/logic.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+
+
+    $(".typed").each(function () {
+        var typed = new Typed('.typed', {
+            stringsElement: '.typed-strings',
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 1500,
+        });
+    });
+    $(".parallax").each(function () {
+        $(this).parallaxie({
+            speed: 0.5,
+        });
+    });
+});
+
+
+
+
+
